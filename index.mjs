2027c6c671a4e65dd5899234c388a444d4824ef8
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-09-03 21:35:42 +0200

    Updated

--- index.mjs
+++ index.mjs
@@ -1,4 +1,15 @@
-const scanner = require('local-network-scanner');
-scanner.scan({arguments: ["-I", "en0"]}, devices => {
-    console.log(devices);
-});
+import scanner from 'local-network-scanner'
+
+async function scan() {
+  let promise = await new Promise((resolve, reject) => {
+    scanner.scan({arguments: ["-I", "en0"]}, devices => {
+      resolve(devices)
+    })
+  }).catch(err => {throw err})
+  return promise
+}
+
+(async function() {
+  let devices = await scan()
+  console.log(devices)
+})()
