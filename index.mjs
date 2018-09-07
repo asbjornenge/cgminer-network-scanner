5972a851f57a77f04580546bcca63e955ff0f882
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-09-07 13:36:11 +0200

    Updates

--- index.mjs
+++ index.mjs
@@ -1,4 +1,8 @@
 import scanner from 'local-network-scanner'
+import tryport from 'is-port-reachable'
+import cgminer from 'cgminer-api'
+
+const CGMINER_PORT = 4028
 
 async function scan() {
   let promise = await new Promise((resolve, reject) => {
@@ -11,5 +15,16 @@ async function scan() {
 
 (async function() {
   let devices = await scan()
-  console.log(devices)
+  let miners = []
+  for (let device of devices) {
+    let open = await tryport(CGMINER_PORT, { host: device.ip })
+    if (!open) continue;
+    let cgc = new cgminer.Client({
+      host: device.ip,
+      port: CGMINER_PORT
+    })
+    let config = await cgminer.config()
+    miners.push(device.ip, config)
+  }
+  console.log(miners)
 })()
