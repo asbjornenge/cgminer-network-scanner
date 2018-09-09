286f2fd62933b4a22b40327958cc62d3382f464a
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-09-09 23:59:54 +0200

    Added unique

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = async function(args) {
   for (let ip of ips) {
     let open = await tryport(args.cgminer_port, { host: ip })
     if (!open) continue
+    if (miners.map(m => m.ip).indexOf(ip) >= 0) continue
     let config = await cgminer.config(ip, args.cgminer_port)
     miners.push({
       ip: ip,
