ecf03af0e8f67bfae93535ef9b0b1ac9ad7fe9a9
Asbjørn Enge <asbjorn@hanafjedle.net>
2018-09-12 09:58:06 +0200

    Support any verb

--- cgminer.js
+++ cgminer.js
@@ -1,9 +1,9 @@
 const rpc = require('miner-rpc')
 
-module.exports.config = async function(ip, port) {
+module.exports = async function(method, ip, port) {
   let promise = await new Promise((resolve, reject) => {
     let client = rpc.client(ip, port)
-    client.get('config', function(err, data) {
+    client.get(method, function(err, data) {
       if (err) reject(err)
       else resolve(data)
     })
